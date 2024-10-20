b0de339167a29a85c0c6068e1e924666d9bc446c
woaitimitimi <setllajiajia@outlook.com>
2024-10-20 23:44:01 +1100

    hide

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBQ2UG_Ep0Vmc9gMXVRutN-oOHYfOFHDjU",
-  authDomain: "week7-sijia.firebaseapp.com",
-  projectId: "week7-sijia",
-  storageBucket: "week7-sijia.appspot.com",
-  messagingSenderId: "213586902068",
-  appId: "1:213586902068:web:3c1e949b3118f372ad5e61"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 }
 
 initializeApp(firebaseConfig)
