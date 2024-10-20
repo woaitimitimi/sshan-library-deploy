3d0a9767f89b2fc5eaf22d698c321bb0a6c550f7
woaitimitimi <setllajiajia@outlook.com>
2024-10-20 22:42:43 +1100

    trigger3

--- vite.config.js
+++ vite.config.js
@@ -6,17 +6,16 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.CF_PAGES 
-  ? '/' // Cloudflare Pages
-  : process.env.GITHUB_ACTIONS 
-    ? '/sshan-library-deploy/' // GitHub Pages
-    : '/', //local
-    
+  base: '',
+  build: {
+    outDir: 'dist'
+  },
+  
   plugins: [
     vue(),
     vueDevTools(),
   ],
-  
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
